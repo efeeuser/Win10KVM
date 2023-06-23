51ee80a7ed57d29ba6681f208fa03316bfff3615
efeeuser <137563574+efeeuser@users.noreply.github.com>
2023-06-24 01:10:36 +0300

    Update qemu_and_img,iso.sh

--- qemu_and_img,iso.sh
+++ qemu_and_img,iso.sh
@@ -1,3 +1,10 @@
+echo Downloading QEMU
 sudo apt update && sudo apt install qemu-system-x86-64 -y
+clear
+Getting Iso
 wget https://ss2.softlay.com/files/en_windows_10_22h2_x64_dvd.iso
+clear
+Creating IMG File
 qemu-img create w10.img 40G
+echo Done!
+clear
